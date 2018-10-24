45b0e1d2f78f4dac186d029844b2fe5d7368205b
Iasmiimspf <iasmiimspferreira@gmail.com>
2018-10-24 08:08:36 -0400

    Adição arquivos iniciais

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+     <title>Ana e Mia</title>
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+
+</body>
---
+++ login_inicial.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="utf-8">
+<title>Clube de Ciências</title>
+
+	<!-- Custom styles for this template -->
+	<link href="style.css" rel="stylesheet">
+
+	<link href="bootstrap-4.1.3-dist/css/bootstrap.css" rel="stylesheet">
+
+</head>
+<body>
+
+<!--Login -->
+	<div class="card card-form">
+	  <div class="card-body card-body-form">
+	    <form>
+		  <div class="form-group">
+		    <div class="dropdown">
+			  <div class="form-group col-md-12">
+                              <label for="inputState">Acesso</label>
+                              <select id="inputState" class="form-control" name="acesso">
+                                <option selected value="0">...</option>
+                                <option value="Estudante">Estudante</option>
+                                <option value="Professor">Professor</option>
+                              </select>
+                            </div>
+                          </div>
+		  </div>
+		  <div class="form-group col-md-12">
+		    <label for="exampleInputPassword1">RA</label>
+		    <input type="ra" class="form-control" id="exampleInputPassword1" placeholder="RA" name="ra">
+		  </div>
+		  
+		  <button type="submit" class="btn btn-primary botao">Entrar</button>
+		</form>
+	  </div>
+	</div>
+<!-- Fim Login -->
+
+<!--Intro -->
+	<div class="card-intro-img">
+	  <img class="card-img-top" src="img/2.png" alt="Card image cap">
+	</div>
+		
+	  <div class="card-body card-intro">
+	    <h5 class="card-title card-text-intro">Faça Parte!</h5>
+		 <p class="card-text card-text-intro">Você que é estudante do IFMS Campus Corumbá, entre e acesse a conteúdos exclusivos.</br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at varius neque. Phasellus condimentum nisi ipsum, vel porta eros venenatis in. Donec in eros nec elit congue pellentesque sed ac lacus. Etiam id erat suscipit, viverra lectus quis, dignissim nisi. Vestibulum at metus in nisl cursus pulvinar eget et dui. Duis quis lacinia arcu, sit amet bibendum orci. Duis feugiat viverra pharetra. Fusce eu condimentum turpis. Suspendisse potenti. Nam blandit odio dapibus dui vestibulum sagittis. Ut gravida neque ac varius dictum. Nunc elementum egestas turpis et cursus. Vivamus hendrerit ante vel maximus pharetra. Mauris vitae pretium ipsum. Vivamus quis velit ultricies, semper risus in, porttitor velit.</p>
+   
+	  </div>
+
+<!--Fim Intro -->
+
+
+</body>
